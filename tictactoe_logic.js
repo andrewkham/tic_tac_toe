79968fd8019680e14bbf818e44d2fd60d907b432
Andrew Ham <hampossible@gmail.com>
2015-05-12 23:38:07 -0700

    finalized win logic added ring count and reload command

--- tictactoe_logic.js
+++ tictactoe_logic.js
@@ -1,82 +1,122 @@
 var cells = document.getElementsByClassName("cell");
-
 var turn = 0;
+var Lakers = '<img src="images/lakers_logo_mini.png">';
+var Celtics = '<img src="images/celtics_logo_mini.gif">';
+var Winner = "";
+var rings = document.getElementsByClassName("ring");
+var lakersRings = 0;
+var celticsRings = 0;
+var lakersRing = '<img src="images/lakers_ring_mini.jpeg" style = "margin-bottom: 23px">';
+var celticsRing = '<img src="images/celtics_ring_mini.jpeg" style = "margin-bottom: 18px">';
 
 function takeTurns() {
     turn++;
     if ((turn + 2) % 2 === 0) {
-        return "<img src='images/lakers_logo_mini.png'>";
-        return "Lakers";
-    } else return "<img src='images/celtics_logo_mini.gif'>";
-    return "Celtics";
+        return Lakers;
+    } else return Celtics;
 }
 
-chooseBox(); 
+chooseBox();
 
 function chooseBox() {
-        for (var i = 0; i < cells.length; i++) {
-            cells[i].addEventListener('click', function() {
-                if (this.innerHTML !== "") {
-                    alert ("Sorry bitch, box is taken!")
-                } else this.innerHTML = takeTurns();
-            })
-        }
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('click', function() {
+            if (this.innerHTML !== "") {
+                alert("Box taken!");
+            } else this.innerHTML = takeTurns();
+            getWinner();
+        })
     }
-
-
-
-
-
-//================================== WIN LOGIC =========================================//
-// function cells(key) {
-
-//     this.innerHTML = 'x';
-// }
+}
 
 function getWinner() {
-    if (isWinner('x')) {
-        return 'x';
+    // Lakers // 
+    if (((cells[0].innerHTML == Lakers) && (cells[1].innerHTML == Lakers) && (cells[2].innerHTML == Lakers)) ||
+        ((cells[3].innerHTML == Lakers) && (cells[4].innerHTML == Lakers) && (cells[5].innerHTML == Lakers)) ||
+        ((cells[6].innerHTML == Lakers) && (cells[7].innerHTML == Lakers) && (cells[8].innerHTML == Lakers)) ||
+        ((cells[0].innerHTML == Lakers) && (cells[3].innerHTML == Lakers) && (cells[6].innerHTML == Lakers)) ||
+        ((cells[1].innerHTML == Lakers) && (cells[4].innerHTML == Lakers) && (cells[7].innerHTML == Lakers)) ||
+        ((cells[2].innerHTML == Lakers) && (cells[5].innerHTML == Lakers) && (cells[8].innerHTML == Lakers)) ||
+        ((cells[0].innerHTML == Lakers) && (cells[4].innerHTML == Lakers) && (cells[8].innerHTML == Lakers)) ||
+        ((cells[6].innerHTML == Lakers) && (cells[4].innerHTML == Lakers) && (cells[2].innerHTML == Lakers))) {
+        Winner = Lakers;
+        countScore();
+        updateScoreboard();
+        boardReset();
     }
-    if (isWinner('o')) {
-        return 'o';
+    // Celtics //
+    if (((cells[0].innerHTML == Celtics) && (cells[1].innerHTML == Celtics) && (cells[2].innerHTML == Celtics)) ||
+        ((cells[3].innerHTML == Celtics) && (cells[4].innerHTML == Celtics) && (cells[5].innerHTML == Celtics)) ||
+        ((cells[6].innerHTML == Celtics) && (cells[7].innerHTML == Celtics) && (cells[8].innerHTML == Celtics)) ||
+        ((cells[0].innerHTML == Celtics) && (cells[3].innerHTML == Celtics) && (cells[6].innerHTML == Celtics)) ||
+        ((cells[1].innerHTML == Celtics) && (cells[4].innerHTML == Celtics) && (cells[7].innerHTML == Celtics)) ||
+        ((cells[2].innerHTML == Celtics) && (cells[5].innerHTML == Celtics) && (cells[8].innerHTML == Celtics)) ||
+        ((cells[0].innerHTML == Celtics) && (cells[4].innerHTML == Celtics) && (cells[8].innerHTML == Celtics)) ||
+        ((cells[6].innerHTML == Celtics) && (cells[4].innerHTML == Celtics) && (cells[2].innerHTML == Celtics))) {
+        Winner = Celtics;
+        countScore();
+        updateScoreboard();
+        boardReset();
     }
-    return null;
+    // Cat's Game // 
+    if (((cells[0].innerHTML !== "") && (cells[1].innerHTML !== "") && (cells[2].innerHTML !== "")) &&
+        ((cells[3].innerHTML !== "") && (cells[4].innerHTML !== "") && (cells[5].innerHTML !== "")) &&
+        ((cells[6].innerHTML !== "") && (cells[7].innerHTML !== "") && (cells[8].innerHTML !== ""))) {
+        alert("DRAW!")
+        countScore();
+        boardReset();
+    }
 }
 
-function isWinner(player) {
-    return winsRow(player) || winsColumn(player) || winsDiagonal(player);
+function boardReset() {
+    for (i = 0; i <= cells.length; i++) {
+        cells[i].innerHTML = "";
+    }
 }
 
-function winsRow(player) {
-    return allThree(player, cells('a'), cells('b'), cells('c')) ||
-        allThree(player, cells('d'), cells('e'), cells('f')) ||
-        allThree(player, cells('g'), cells('h'), cells('i'));
+function countScore() {
+    if (Winner == Lakers) {
+        lakersRings++;
+    }
+    if (Winner == Celtics) {
+        celticsRings++;
+    }
 }
 
-function winsColumn(player) {
-    return allThree(player, cells('a'), cells('d'), cells('g')) ||
-        allThree(player, cells('b'), cells('e'), cells('h')) ||
-        allThree(player, cells('c'), cells('f'), cells('i'));
+function updateScoreboard() {
+    if (lakersRings == 1) {
+        rings[0].innerHTML = lakersRing;
+    }
+    if (lakersRings==2) {
+        rings[1].innerHTML = lakersRing;
+    }
+    if (lakersRings==3) {
+        rings[2].innerHTML = lakersRing;
+    }
+    if (lakersRings==4) {
+        rings[3].innerHTML = lakersRing;
+    }
+    if (lakersRings==5) {
+        rings[4].innerHTML = lakersRing;
+        alert("Lakers are NBA Champions!")
+        location.reload();
+    }
+    if (celticsRings==1) {
+        rings[5].innerHTML = celticsRing;
+    }
+    if (celticsRings==2) {
+        rings[6].innerHTML = celticsRing;
+    }
+    if (celticsRings==3) {
+        rings[7].innerHTML = celticsRing;
+    }
+    if (celticsRings==4) {
+        rings[8].innerHTML = celticsRing;
+    }
+    if (celticsRings==5) {
+        rings[9].innerHTML = celticsRing;
+        alert("Celtics are NBA Champions!")
+        location.reload();
+    }
 }
 
-function winsDiagonal(player) {
-    return allThree(player, cells('a'), cells('e'), cells('i')) ||
-        allThree(player, cells('c'), cells('e'), cells('g'));
-}
-
-function allThree(player, cell_one, cell_two, cell_three) {
-    return (cell_one === player) && (cell_two === player) && (cell_three === player);
-}
-
-//===================================Linkage=============================================//
-
-// document.getElementById("cellA").addEventListener("click", cells)
-//   {case 'a': return 'x'});
-// document.getElementById("#cellB").addEventListener("click", cells(key){case 'b': return 'x'});
-// document.getElementById("#cellC").addEventListener("click", cells(key){case 'c': return 'x'});
-// document.getElementById("#cellD").addEventListener("click", cells(key){case 'd': return 'x'});
-// document.getElementById("#cellE").addEventListener("click", cells(key){case 'e': return 'x'});
-// document.getElementById("#cellF").addEventListener("click", cells(key){case 'f': return 'x'});
-// document.getElementById("#cellG").addEventListener("click", cells(key){case 'g': return 'x'});
-// document.getElementById("#cellH").addEventListener("click", cells(key){case 'h': return 'x'});
-// document.getElementById("#cellI").addEventListener("click", cells(key){case 'i': return 'x'});
